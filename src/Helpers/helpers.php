14b586b4a8aa80a3c582001d139fb750cc2b44b4
talhak55 <talhakeskin55@gmail.com>
2025-01-24 22:46:44 +0100

    Initial

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -12,3 +12,17 @@ if (!function_exists('OrganizationService')) {
         return Container::getInstance()->make('OrganizationService');
     }
 }
+
+if (!function_exists('FileStorageService')) {
+    function FileStorageService()
+    {
+        return Container::getInstance()->make('FileStorageService');
+    }
+}
+
+if (!function_exists('KeyService')) {
+    function KeyService()
+    {
+        return Container::getInstance()->make('KeyService');
+    }
+}
