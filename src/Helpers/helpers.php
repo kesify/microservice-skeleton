75612b48a4dc4e007303938dac4110b9ec41bcee
talhak55 <talhakeskin55@gmail.com>
2025-01-24 22:52:49 +0100

    Initial

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -3,26 +3,54 @@
 use Illuminate\Container\Container;
 
 if (!function_exists('OrganizationService')) {
-
     /**
+     * Get the OrganizationService instance from the container.
+     *
+     * @return \Kesify\MicroserviceSkeleton\Services\OrganizationService
      * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
-    function OrganizationService()
+    function OrganizationService(): mixed
     {
-        return Container::getInstance()->make('OrganizationService');
+        try {
+            return Container::getInstance()->make('OrganizationService');
+        } catch (\Illuminate\Contracts\Container\BindingResolutionException $e) {
+            // Optional: Log or handle the exception
+            throw $e;
+        }
     }
 }
 
 if (!function_exists('FileStorageService')) {
-    function FileStorageService()
+    /**
+     * Get the FileStorageService instance from the container.
+     * @return \Kesify\MicroserviceSkeleton\Services\FileStorageService
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    function FileStorageService(): mixed
     {
-        return Container::getInstance()->make('FileStorageService');
+        try {
+            return Container::getInstance()->make('FileStorageService');
+        } catch (\Illuminate\Contracts\Container\BindingResolutionException $e) {
+            // Optional: Log or handle the exception
+            throw $e;
+        }
     }
 }
 
 if (!function_exists('KeyService')) {
-    function KeyService()
+    /**
+     * Get the KeyService instance from the container.
+     *
+     * @return \Kesify\MicroserviceSkeleton\Services\KeyService
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    function KeyService(): mixed
     {
-        return Container::getInstance()->make('KeyService');
+        try {
+            return Container::getInstance()->make('KeyService');
+        } catch (\Illuminate\Contracts\Container\BindingResolutionException $e) {
+            // Optional: Log or handle the exception
+            throw $e;
+        }
     }
 }
