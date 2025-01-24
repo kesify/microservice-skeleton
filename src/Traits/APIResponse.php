ab0037fa6627867a2b9daf1ec0fdd4a7d18d9ecb
talhak55 <talhakeskin55@gmail.com>
2025-01-24 11:25:31 +0100

    Initial

---
+++ src/Traits/APIResponse.php
@@ -0,0 +1,275 @@
+<?php
+
+namespace Kesify\MicroserviceSkeleton\Traits;
+
+
+use Error;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Response;
+
+trait APIResponse{
+
+    /**
+     * Return generic json response with the given data.
+     *
+     * @param array $data
+     * @param int $statusCode
+     * @param array $headers
+     *
+     * @return JsonResponse
+     */
+    protected function apiResponse(array $data = [], int $statusCode = 200, array $headers = []): JsonResponse
+    {
+        $result = $this->parseGivenData($data, $statusCode, $headers);
+
+        return Response::json(
+            $result['content'], $result['statusCode'], $result['headers']
+        );
+    }
+
+    /**
+     * @param JsonResource $resource
+     * @param null $message
+     * @param int $statusCode
+     * @param array $headers
+     */
+    protected function respondWithResource(JsonResource $resource, $message = null, int $statusCode = 200, array $headers = []): JsonResponse
+    {
+        return $this->apiResponse(
+            [
+                'success' => true,
+                'result' => $resource,
+                'message' => $message
+            ], $statusCode, $headers
+        );
+    }
+
+    /**
+     * @param array $data
+     * @param int $statusCode
+     * @param array $headers
+     * @return array
+     */
+    public function parseGivenData(array $data = [], int $statusCode = 200, array $headers = []): array
+    {
+        $responseStructure = [
+            'message' => $data['message'] ?? ($data['success'] ? 'success' : 'failed'),
+            'result' => $data['result'] ?? null,
+        ];
+
+        if (isset($data['errors'])) {
+            $responseStructure['errors'] = $data['errors'];
+        }
+
+        if (isset($data['alerts'])) {
+            $responseStructure['alerts'] = $data['alerts'];
+        }
+
+        $statusCode = $data['status'] ?? $statusCode;
+
+        if (isset($data['exception']) && ($data['exception'] instanceof Error || $data['exception'] instanceof Exception)) {
+            if (Config::get('app.env') !== 'production') {
+                $responseStructure['exception'] = [
+                    'message' => $data['exception']->getMessage(),
+                    'file' => $data['exception']->getFile(),
+                    'line' => $data['exception']->getLine(),
+                    'code' => $data['exception']->getCode(),
+                    'trace' => $data['exception']->getTraceAsString(), // Use getTraceAsString for better readability
+                ];
+            }
+
+            $statusCode = ($statusCode === 200) ? 500 : $statusCode;
+        }
+
+        if (isset($data['success']) && $data['success'] === false) {
+            $responseStructure['message'] = $responseStructure['message'] ?? 'failed';
+            $responseStructure['error_code'] = $data['error_code'] ?? 'K-0';
+        }
+
+        return ["content" => $responseStructure, "statusCode" => $statusCode, "headers" => $headers];
+    }
+
+
+
+    /*
+     *
+     * Just a wrapper to facilitate abstract
+     */
+
+    /**
+     * @param ResourceCollection $resourceCollection
+     * @param null $message
+     * @param int $statusCode
+     * @param array $headers
+     * @return JsonResponse
+     */
+    protected function respondWithResourceCollection(ResourceCollection $resourceCollection, $message = null, int $statusCode = 200, array $headers = []): JsonResponse
+    {
+        return $this->apiResponse(
+            [
+                'success' => true,
+                'result' => $resourceCollection->response()->getData()
+            ], $statusCode, $headers
+        );
+    }
+
+    /**
+     * Respond with success.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondSuccess(string $message = ''): JsonResponse
+    {
+        return $this->apiResponse(['success' => true, 'message' => $message]);
+    }
+
+    /**
+     * Respond with created.
+     *
+     * @param $data
+     *
+     * @return JsonResponse
+     */
+    protected function respondCreated($data): JsonResponse
+    {
+        return $this->apiResponse($data, 201);
+    }
+
+    /**
+     * Respond with no content.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondNoContent(string $message = 'No Content Found'): JsonResponse
+    {
+        return $this->apiResponse(['success' => false, 'message' => $message], 200);
+    }
+
+    /**
+     * Respond with no content.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondNoContentResource(string $message = 'No Content Found'): JsonResponse
+    {
+        return $this->respondWithResource(([]), $message);
+    }
+    /**
+     * Respond with no content.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondNoContentResourceCollection(string $message = 'No Content Found'): JsonResponse
+    {
+        return $this->respondWithResourceCollection(([]), $message);
+    }
+
+    /**
+     * Respond with unauthorized.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondUnAuthorized(string $message = 'Unauthorized'): JsonResponse
+    {
+        return $this->respondError($message, 401);
+    }
+
+    /**
+     * Respond with error.
+     *
+     * @param $message
+     * @param int $statusCode
+     *
+     * @param Exception|null $exception
+     * @param bool|null $error_code
+     * @return JsonResponse
+     */
+    protected function respondError(?string $message, int $statusCode = 400, ?Exception $exception = null, int $error_code = 1): JsonResponse
+    {
+        return $this->apiResponse(
+            [
+                'success' => false,
+                'message' => $message ?? 'There was an internal error, Pls try again later',
+                'exception' => $exception,
+                'error_code' => $error_code
+            ], $statusCode
+        );
+    }
+
+    /**
+     * Respond with forbidden.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondForbidden(string $message = 'Forbidden'): JsonResponse
+    {
+        return $this->respondError($message, 403);
+    }
+
+    /**
+     * Respond with not found.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondNotFound(string $message = 'Not Found'): JsonResponse
+    {
+        return $this->respondError($message, 404);
+    }
+
+    // /**
+    //  * Respond with failed login.
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // protected function respondFailedLogin()
+    // {
+    //     return $this->apiResponse([
+    //         'errors' => [
+    //             'email or password' => 'is invalid',
+    //         ]
+    //     ], 422);
+    // }
+
+    /**
+     * Respond with internal error.
+     *
+     * @param string $message
+     *
+     * @return JsonResponse
+     */
+    protected function respondInternalError(string $message = 'Internal Error'): JsonResponse
+    {
+        return $this->respondError($message, 500);
+    }
+
+    protected function respondValidationErrors(ValidationException $exception): JsonResponse
+    {
+        return $this->apiResponse(
+            [
+                'success' => false,
+                'message' => $exception->getMessage(),
+                'errors' => $exception->errors()
+            ],
+            422
+        );
+    }
+
+}
