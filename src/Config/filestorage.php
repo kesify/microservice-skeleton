d4b6088f756fede737bbe479776754a28026512e
talhak55 <talhakeskin55@gmail.com>
2025-07-07 03:44:06 +0200

    Changes

--- src/Config/filestorage.php
+++ src/Config/filestorage.php
@@ -11,6 +11,26 @@ return [
             'markAsInactive' => false,
             'deleteRestOnSameConfiguration'=>false,
             //'afterUpload' => [\App\Http\Controllers\Controller::class, 'handleUpload'],
+        ],
+        'module-image'=>[
+            'name'=>'default',
+            'connection'=>'main',
+            'disk'=>'s3-public',
+            'path'=>'module/{{module_id}}/{{generated_filename}}.{{extension}}',
+            'maxSize'=>10000000, //1mb
+            'markAsInactive' => false,
+            'deleteRestOnSameConfiguration'=>false,
+            //'afterUpload' => [\App\Http\Controllers\Controller::class, 'handleUpload'],
+        ],
+        'module-screenshots'=>[
+            'name'=>'default',
+            'connection'=>'main',
+            'disk'=>'s3-public',
+            'path'=>'module/{{module_id}}/screenshots/{{generated_filename}}.{{extension}}',
+            'maxSize'=>10000000, //1mb
+            'markAsInactive' => false,
+            'deleteRestOnSameConfiguration'=>false,
+            //'afterUpload' => [\App\Http\Controllers\Controller::class, 'handleUpload'],
         ]
     ]
 ];
